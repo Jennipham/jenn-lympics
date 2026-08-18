4e4c2f23b3af103bf11086e439879e97a340d7af
Jennifer Pham <30297540+Jennipham@users.noreply.github.com>
2026-08-19 00:01:58 +0100

    Update players.ts

--- jenn-lympics-website/src/components/Teams/players.ts
+++ jenn-lympics-website/src/components/Teams/players.ts
@@ -17,7 +17,6 @@ export const players = [
         { name: "Michael I", paid: true },
         { name: "EJ", paid: true },
         { name: "Anas", paid: false },
-        { name: "Big Abdi", paid: false },
         { name: "Tyler", paid: false },
         { name: "Samir", paid: false },
         { name: "Ibrahim", paid: false },
